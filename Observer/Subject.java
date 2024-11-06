05fe4c53ec3fe761cf26688bd2a21a2b7e9060ba
Kavishka Kalhara <kavikalhara9h@gmail.com>
2024-11-06 14:31:44 +0530

    Observer Pattern added

---
+++ Observer/Subject.java
@@ -0,0 +1,9 @@
+// Subject.java
+import java.util.ArrayList;
+import java.util.List;
+
+public interface Subject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
+}
